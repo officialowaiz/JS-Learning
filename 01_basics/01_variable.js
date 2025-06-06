61916c0589c3ca21cd6ef0184b97de0b341cd4b0
officialowaiz <owaizkhan0001@gmail.com>
2025-06-06 13:25:45 +0000

    add variable file

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 112211
+let accountEmail = "owaiz@gm.com"
+var accountPassword = "owaiz123"
+accountCity = "amravati"
+let accountState;
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
