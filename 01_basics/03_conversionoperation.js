d5f654ac1a6cacaef82e21697898ba4d6c00885d
officialowaiz <owaizkhan0001@gmail.com>
2025-06-12 10:14:05 +0000

    lecture 6

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let score = "Owaiz"
+console.log(typeof score);
+console.log(typeof(score));
+
+//data-type conversion
+
+
+// 1) string to number 
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+// 2) String to boolean
+
+let valueInBoolean = Boolean(score)
+console.log(typeof(valueInBoolean))
+console.log(valueInBoolean)
